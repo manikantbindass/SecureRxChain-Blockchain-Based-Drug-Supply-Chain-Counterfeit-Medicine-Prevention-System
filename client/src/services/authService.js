fd03bbcd61051d566f4ef7612b8c9f5fd1cadb2e
Manikant Bindass <manikantbindass@rediffmail.com>
2026-03-13 14:08:55 +0530

    feat(client): add authService.js - auth API methods

---
+++ client/src/services/authService.js
@@ -0,0 +1,11 @@
+// client/src/services/authService.js
+import api from './api';
+
+const authService = {
+  register: (data) => api.post('/auth/register', data),
+  login: (data) => api.post('/auth/login', data),
+  getProfile: () => api.get('/auth/profile'),
+  updateProfile: (data) => api.put('/auth/profile', data),
+};
+
+export default authService;
